ac95a2123460bd5ea9f6d5afb07a9629d3b15b34
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-09-23 16:20:44 -0700

    Updates

---
+++ precache-manifest.3b1ae2761017846da28666db3a54ba84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae3ca4cd494161fc92b359d154083b5",
+    "url": "/Adyen-Payment-Trial/index.html"
+  },
+  {
+    "revision": "2dc051f2fd220ae1a8a7",
+    "url": "/Adyen-Payment-Trial/static/css/main.e4f683f8.chunk.css"
+  },
+  {
+    "revision": "d9790f3b5cdf14cd7fca",
+    "url": "/Adyen-Payment-Trial/static/js/2.8bdf54b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Adyen-Payment-Trial/static/js/2.8bdf54b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc051f2fd220ae1a8a7",
+    "url": "/Adyen-Payment-Trial/static/js/main.780e10df.chunk.js"
+  },
+  {
+    "revision": "f59c448edfebfcaaae74",
+    "url": "/Adyen-Payment-Trial/static/js/runtime-main.56cf97b9.js"
+  }
+]);
